4f63bdf6bf56a64b7d578051293ee7471c7b2b46
DevExpressExampleBot <fakeEmail>
2024-09-17 13:48:10 +0400

    Source auto update [skip ci]

--- VB/EncryptionExample/Program.vb
+++ VB/EncryptionExample/Program.vb
@@ -1,90 +1,82 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.Diagnostics
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports DevExpress.Spreadsheet
 Imports DevExpress.XtraSpreadsheet
 
 Namespace EncryptionExample
-	Friend Class Program
-		Private Shared Property IsValid() As Boolean
-		Shared Sub Main(ByVal args() As String)
-			Dim workbook As New Workbook()
-			workbook.Options.Import.Password = "123"
-			workbook.LoadDocument("Documents\encrypted.xlsx")
 
-			AddHandler workbook.EncryptedFilePasswordRequest, AddressOf Workbook_EncryptedFilePasswordRequest
-			AddHandler workbook.EncryptedFilePasswordCheckFailed, AddressOf Workbook_EncryptedFilePasswordCheckFailed
-			AddHandler workbook.InvalidFormatException, AddressOf Workbook_InvalidFormatException
+    Friend Class Program
 
-			Dim encryptionOptions As New EncryptionSettings()
-			encryptionOptions.Type = EncryptionType.Strong
-			encryptionOptions.Password = "12345"
+        Private Shared Property IsValid As Boolean
 
-			Console.WriteLine("Select the file format: XLSX/XLS/XLSB")
-			Dim answerFormat As String = Console.ReadLine()?.ToLower()
-			Dim documentFormat As DocumentFormat = DocumentFormat.Undefined
-			Select Case answerFormat
-				Case "xlsx"
-					documentFormat = DocumentFormat.OpenXml
-				Case "xls"
-					documentFormat = DocumentFormat.Xls
-				Case "xlsb"
-					documentFormat = DocumentFormat.Xlsb
-			End Select
-			Dim fileName As String = String.Format("EncryptedwithNewPassword.{0}", answerFormat)
-			workbook.SaveDocument(fileName, documentFormat, encryptionOptions)
+        Shared Sub Main(ByVal args As String())
+            Dim workbook As Workbook = New Workbook()
+            workbook.Options.Import.Password = "123"
+            workbook.LoadDocument("Documents\encrypted.xlsx")
+            AddHandler workbook.EncryptedFilePasswordRequest, AddressOf Workbook_EncryptedFilePasswordRequest
+            AddHandler workbook.EncryptedFilePasswordCheckFailed, AddressOf Workbook_EncryptedFilePasswordCheckFailed
+            AddHandler workbook.InvalidFormatException, AddressOf Workbook_InvalidFormatException
+            Dim encryptionOptions As EncryptionSettings = New EncryptionSettings()
+            encryptionOptions.Type = EncryptionType.Strong
+            encryptionOptions.Password = "12345"
+            Console.WriteLine("Select the file format: XLSX/XLS/XLSB")
+            Dim answerFormat As String = Console.ReadLine()?.ToLower()
+            Dim documentFormat As DocumentFormat = DocumentFormat.Undefined
+            Select Case answerFormat
+                Case "xlsx"
+                    documentFormat = DocumentFormat.OpenXml
+                Case "xls"
+                    documentFormat = DocumentFormat.Xls
+                Case "xlsb"
+                    documentFormat = DocumentFormat.Xlsb
+            End Select
 
-			If IsValid = True Then
-				workbook.SaveDocument(fileName, documentFormat)
-				Process.Start(fileName)
-			End If
+            Dim fileName As String = String.Format("EncryptedwithNewPassword.{0}", answerFormat)
+            workbook.SaveDocument(fileName, documentFormat, encryptionOptions)
+            If IsValid = True Then
+                workbook.SaveDocument(fileName, documentFormat)
+                Call Process.Start(fileName)
+            End If
 
-			Console.WriteLine("The document is saved with new password. Continue? (y/n)")
-			Dim answer As String = Console.ReadLine()?.ToLower()
+            Console.WriteLine("The document is saved with new password. Continue? (y/n)")
+            Dim answer As String = Console.ReadLine()?.ToLower()
+            If Equals(answer, "y") Then
+                Console.WriteLine("Re-opening the file...")
+                workbook.LoadDocument(fileName)
+            End If
+        End Sub
 
-			If answer = "y" Then
-				Console.WriteLine("Re-opening the file...")
-				workbook.LoadDocument(fileName)
-			End If
+        Private Shared Sub Workbook_InvalidFormatException(ByVal sender As Object, ByVal e As SpreadsheetInvalidFormatExceptionEventArgs)
+            Console.WriteLine(e.Exception.Message.ToString() & " Press any key to close...")
+            Console.ReadKey(True)
+        End Sub
 
-		End Sub
+        Private Shared Sub Workbook_EncryptedFilePasswordRequest(ByVal sender As Object, ByVal e As EncryptedFilePasswordRequestEventArgs)
+            Console.WriteLine("Enter password:")
+            e.Password = Console.ReadLine()
+            e.Handled = True
+            IsValid = True
+        End Sub
 
-		Private Shared Sub Workbook_InvalidFormatException(ByVal sender As Object, ByVal e As SpreadsheetInvalidFormatExceptionEventArgs)
-			Console.WriteLine(e.Exception.Message.ToString() & " Press any key to close...")
-			Console.ReadKey(True)
-		End Sub
+        Private Shared Sub Workbook_EncryptedFilePasswordCheckFailed(ByVal sender As Object, ByVal e As EncryptedFilePasswordCheckFailedEventArgs)
+            Select Case e.Error
+                Case SpreadsheetDecryptionError.PasswordRequired
+                    Console.WriteLine("You did not enter the password!")
+                    e.TryAgain = True
+                    e.Handled = True
+                Case SpreadsheetDecryptionError.WrongPassword
+                    Console.WriteLine("The password is incorrect. Try Again? (y/n)")
+                    Dim answer As String = Console.ReadLine()?.ToLower()
+                    If Equals(answer, "y") Then
+                        e.TryAgain = True
+                        e.Handled = True
+                    Else
+                        IsValid = False
+                    End If
 
-		Private Shared Sub Workbook_EncryptedFilePasswordRequest(ByVal sender As Object, ByVal e As EncryptedFilePasswordRequestEventArgs)
-			Console.WriteLine("Enter password:")
-			e.Password = Console.ReadLine()
-			e.Handled = True
-			IsValid = True
-		End Sub
+            End Select
 
-		Private Shared Sub Workbook_EncryptedFilePasswordCheckFailed(ByVal sender As Object, ByVal e As EncryptedFilePasswordCheckFailedEventArgs)
-			Select Case e.Error
-				Case SpreadsheetDecryptionError.PasswordRequired
-					Console.WriteLine("You did not enter the password!")
-					e.TryAgain = True
-					e.Handled = True
-				Case SpreadsheetDecryptionError.WrongPassword
-					Console.WriteLine("The password is incorrect. Try Again? (y/n)")
-					Dim answer As String = Console.ReadLine()?.ToLower()
-					If answer = "y" Then
-						e.TryAgain = True
-						e.Handled = True
-
-					Else
-						IsValid = False
-					End If
-			End Select
-
-			Program.IsValid = False
-		End Sub
-	End Class
-
+            IsValid = False
+        End Sub
+    End Class
 End Namespace
-
